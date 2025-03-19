a12589ec131ac5f1e90f30797416f9fa0c396886
Abiodun Osagie <abiodunosagie8@gmail>
2025-03-19 12:37:02 +0100

    added the delete and add item feature to core data.

---
+++ Devote/Extension/HideKeyboardExtension.swift
@@ -0,0 +1,8 @@
+//
+//  HideKeyboardExtension.swift
+//  Devote
+//
+//  Created by Abiodun Osagie on 19/03/2025.
+//
+
+import Foundation
---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Abiodun Osagie on 19/03/2025.
+//
+
+import Foundation
