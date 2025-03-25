ec942575ab244496eaec8e33a3bcddc75f472272
Abiodun Osagie <abiodunosagie8@gmail>
2025-03-25 18:21:26 +0100

    Toggle composnent and implementation

---
+++ Devote/Style/CheckBoxStyle.swift
@@ -0,0 +1,18 @@
+//
+//  CheckBoxStyle.swift
+//  Devote
+//
+//  Created by Abiodun Osagie on 25/03/2025.
+//
+
+import SwiftUI
+
+struct CheckBoxStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckBoxStyle()
+}
